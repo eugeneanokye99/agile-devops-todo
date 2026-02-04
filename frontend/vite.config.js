03a8364eaabbc5628eeb18ce977dca49c5d7595a
EugeneDokyeAnokye <yawanokye99@gmail.com>
2026-02-04 08:44:21 +0000

    Update Vite configuration to include additional test exclusions

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,5 +8,11 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/test/setup.js',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+      '**/.{idea,git,cache,output,temp}/**',
+    ],
   },
 })
